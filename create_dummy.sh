c2205976430b4a8e4f92294d1bc08482d25243c2
Rio Fujita <rio_github@rio.st>
2020-07-21 16:50:27 +0900

    Update create_dummy.sh

--- create_dummy.sh
+++ create_dummy.sh
@@ -1,1 +1,11 @@
-for line in $(seq 1000); do buf=""; for i in {0..23}; do for j in 00 10 20 30 40 50; do rnd=$RANDOM; tmp=$(echo "scale=2; 10 + $rnd / 32768 * 40" | bc); buf="$buf,$tmp"; done;done; echo "tag-tmp$line$buf"; done > dummy.data
+for line in $(seq 1000); do
+    buf="";
+    for i in {0..23}; do
+        for j in 00 10 20 30 40 50; do
+            rnd=$RANDOM;
+            tmp=$(echo "scale=2; 10 + $rnd / 32768 * 40" | bc);
+            buf="$buf,$tmp";
+        done;
+    done;
+    echo "tag-tmp$line$buf";
+done > dummy.data
